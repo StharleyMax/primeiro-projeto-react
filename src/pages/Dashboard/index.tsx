e8b02f774f30ec4f34ce685a9881fbd68859fffb
Stharley Maxwell <stharleymax@gmail.com>
2020-10-03 10:16:20 -0300

    css dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no gitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore Repositorios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="">
+          <img
+            src="https://avatars2.githubusercontent.com/u/11269735?s=460&v=4"
+            alt="Stharley Maxwell"
+          />
+          <div>
+            <strong>Desafio</strong>
+            <p>testando </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="">
+          <img
+            src="https://avatars2.githubusercontent.com/u/11269735?s=460&v=4"
+            alt="Stharley Maxwell"
+          />
+          <div>
+            <strong>Desafio</strong>
+            <p>testando </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="">
+          <img
+            src="https://avatars2.githubusercontent.com/u/11269735?s=460&v=4"
+            alt="Stharley Maxwell"
+          />
+          <div>
+            <strong>Desafio</strong>
+            <p>testando </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
